d03034f8be10a626299c5ffc1cb2b921dbe83f85
Nasir Uddin <monforingnasir@gmail.com>
2021-02-25 17:26:50 +0600

    Animal project but use comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Animals Border</h1>
+        <Animals></Animals>
+
       </header>
     </div>
   );
+}
+
+function Animals() {
+  const [animals, setAnimals] = useState([]);
+  const [pet, setPet] = useState(0);
+  useEffect(async () => {
+    const url = `https://jsonplaceholder.typicode.com/comments`;
+    try {
+      const promise = await fetch(url); const data = await promise.json();
+      setAnimals(data);
+      console.log(data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }, [])
+  return (
+    <div className="animal-list">
+      {
+        animals.map(animal => {
+          return (
+            <div className="Child-div">
+              <h1 className="comment">Comment : {pet}</h1>
+              <button onClick={() => setPet(pet - 1)}>Remove</button>
+              <button onClick={() => setPet(pet + 1)}>Add Comment</button>
+              <h3>ID : {animal.id}</h3>
+              <h1>Name : {animal.name}</h1>
+              <h4>Email : {animal.email}</h4>
+              <h3>Body : {animal.body}</h3>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default App;
